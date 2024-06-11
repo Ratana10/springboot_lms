11d7231101f4385efb3bf20f0a60b3c0162c399c
Ratana10 <sanratana18@gmail.com>
2024-06-11 14:36:13 +0700

    update category Response

--- src/main/java/com/kongsun/leanring/system/category/CategoryController.java
+++ src/main/java/com/kongsun/leanring/system/category/CategoryController.java
@@ -1,5 +1,6 @@
 package com.kongsun.leanring.system.category;
 
+import com.kongsun.leanring.system.exception.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -7,8 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/api/v1/categories")
@@ -19,52 +19,77 @@ public class CategoryController {
 
 
     @GetMapping
-    public ResponseEntity<?> getAll() {
+    public ResponseEntity<ApiResponse> getAll() {
         List<Category> categories = categoryService.getAll();
+
         return ResponseEntity
                 .ok()
-                .body(categories.stream()
-                        .map(categoryMapper::toCategoryDTO)
-                        .toList()
+                .body(ApiResponse.builder()
+                        .data(categories.stream()
+                                .map(categoryMapper::toCategoryDTO)
+                                .toList())
+                        .message("get categories successfully")
+                        .httpStatus(OK.value())
+                        .build()
                 );
 
     }
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody @Valid CategoryDTO dto) {
+    public ResponseEntity<ApiResponse> create(@RequestBody @Valid CategoryDTO dto) {
         Category category = categoryMapper.toCategory(dto);
         category = categoryService.create(category);
         return ResponseEntity
                 .status(CREATED)
-                .body(categoryMapper.toCategoryDTO(category));
+                .body(ApiResponse.builder()
+                        .data(categoryMapper.toCategoryDTO(category))
+                        .message("create category successfully")
+                        .httpStatus(CREATED.value())
+                        .build()
+                );
 
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<?> getById(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse> getById(@PathVariable Long id) {
         Category category = categoryService.getById(id);
         return ResponseEntity
                 .status(FOUND)
-                .body(categoryMapper.toCategoryDTO(category));
+                .body(ApiResponse.builder()
+                        .data(categoryMapper.toCategoryDTO(category))
+                        .message("get category successfully")
+                        .httpStatus(FOUND.value())
+                        .build()
+                );
 
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody @Valid CategoryDTO dto) {
+    public ResponseEntity<ApiResponse> update(@PathVariable Long id, @RequestBody @Valid CategoryDTO dto) {
         Category category = categoryMapper.toCategory(dto);
         category = categoryService.update(id, category);
         return ResponseEntity
                 .ok()
-                .body(categoryMapper.toCategoryDTO(category));
+                .body(ApiResponse.builder()
+                        .data(categoryMapper.toCategoryDTO(category))
+                        .message("update category successfully")
+                        .httpStatus(OK.value())
+                        .build()
+                );
 
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteById(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse> deleteById(@PathVariable Long id) {
         categoryService.deleteById(id);
         return ResponseEntity
                 .status(FOUND)
-                .body("delete successfully");
+                .body(ApiResponse.builder()
+                        .data(null)
+                        .message("delete category successfully")
+                        .httpStatus(OK.value())
+                        .build()
+                );
 
     }
 
