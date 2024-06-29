17392951785f11673e94316d063df286e82e5c82
Ratana10 <sanratana18@gmail.com>
2024-06-29 21:07:07 +0700

    fix(category): update response

--- src/main/java/com/kongsun/leanring/system/category/CategoryResponse.java
+++ src/main/java/com/kongsun/leanring/system/category/CategoryResponse.java
@@ -2,8 +2,12 @@ package com.kongsun.leanring.system.category;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class CategoryResponse {
     private Long id;
     private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
